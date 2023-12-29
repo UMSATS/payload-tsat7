3c32fc22f73a4458681d4744583f8d7b739d40ff
Logan Furedi <furedil@myumanitoba.ca>
2023-12-29 15:38:07 -0600

    Chore: Clean Up Multiplexer Code

--- Payload-MCU/Drivers/TCA9548.c
+++ Payload-MCU/Drivers/TCA9548.c
@@ -6,16 +6,19 @@
  *
  *  Purpose: This is the driver file for the I2C multiplexer, part # TCA9548.
  */
-I
 
+#include "TCA9548.h"
 #include "i2c.h"
+
+#include <stdint.h>
 
 #define TCA9548_ADDR 0x70 				// I2C address of the multiplexer
 
 // returns -1 on error
-int TCA9548_set_i2c_channel(int channel_number) {
-
-	if (channel_number < 0 || channel_number > 7) {
+int TCA9548_set_i2c_channel(int channel_number)
+{
+	if (channel_number < 0 || channel_number > 7)
+	{
 		return -1;
 	}
 
@@ -23,6 +26,12 @@ int TCA9548_set_i2c_channel(int channel_number) {
 	uint8_t command_register[1] = {channel_number};
 
 	// usage: i2c object, device addr, payload, payload size (bytes), timeout (ms)
-	HAL_I2C_Master_Transmit(&hi2c1, TCA9548_ADDR, command_register, 1, 100);
+	HAL_StatusTypeDef status = HAL_I2C_Master_Transmit(&hi2c1, TCA9548_ADDR, command_register, 1, 100);
 
+	if (status != HAL_OK)
+	{
+		return -1;
+	}
+
+	return 0;
 }
---
+++ Payload-MCU/Drivers/TCA9548.h
@@ -0,0 +1,17 @@
+/*
+ * TCA9548A.h
+ *
+ *  Created on: Nov 26, 2023
+ *      Author: Jacob Petersen
+ *
+ *  Purpose: This is the driver file for the I2C multiplexer, part # TCA9548.
+ */
+
+#include <stdint.h>
+
+/**
+ * @brief Sets the I2C channel for the multiplexer.
+ *
+ * @return -1 on error. 0 on success.
+ */
+int TCA9548_set_i2c_channel(int channel_number);
