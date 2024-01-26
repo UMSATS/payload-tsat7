94224a9beb4bdd0501778c0a9b294f05b921ec49
Logan Furedi <furedil@myumanitoba.ca>
2024-01-25 20:39:22 -0600

    Chore: Move power.h to HighLevel
    That's not a hardware peripheral T^T

---
+++ Payload-MCU/Drivers/HighLevel/Inc/power.h
@@ -0,0 +1,16 @@
+/*
+ * power.h
+ *
+ *  Created on: Jan 9, 2024
+ *      Author: Logan Furedi
+ */
+
+#ifndef HIGHLEVEL_INC_POWER_H_
+#define HIGHLEVEL_INC_POWER_H_
+
+typedef enum {
+	OFF = 0,
+	ON
+} Power;
+
+#endif /* HIGHLEVEL_INC_POWER_H_ */
